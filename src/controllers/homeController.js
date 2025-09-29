252d152b5a02d490bb00dbc73fa4d268494d8cdc
Alexander Uzunov <kaialexander.11@gmail.com>
2025-09-29 09:06:31 +0300

    Add home controller

---
+++ src/controllers/homeController.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+//const router = express.Router();
+
+router.get('/', (req, res) => {
+    //res.send('Hello from Express!');
+    res.render('index');
+});
+
+// router.get('/about', (req, res) => {
+//     res.render('about');
+// });
+
+module.exports = router;
+
+
--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 //console.log('Hello from Express!');
+//const path = require('path');
+//const handlebars = require('express-handlebars');
 
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
-
+const homeController = require('./controllers/homeController.js');
 const expressConfig = require('./config/expressConfig.js');
 const handlebarsConfig = require('./config/handlebarsConfig.js');
 
 const app = express();
+const PORT = 5000;
 
 expressConfig(app);
 handlebarsConfig(app);
 
-// This line below will work exactly as the line above!
-//require('./config/expressConfig.js')(app);
-
-
-const PORT = 5000;
-
-// Routes:
-app.get('/', (req, res) => {
-    //res.send('Hello from Express!');
-    res.render('index');
-});
+app.use(homeController);
+//app.get('/', homeController.getHome);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}... `));
+
+
