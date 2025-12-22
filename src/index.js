aa710f597eebb81983e5fea66b6654c27c2dd992
Alexander Uzunov <kaialexander.11@gmail.com>
2025-12-22 10:05:45 +0200

    Add register page

--- src/index.js
+++ src/index.js
@@ -15,14 +15,16 @@ const app = express();
 
 const PORT = 5000;
 
+expressConfig(app);
+handlebarsConfig(app);
+
 dbConnect()
     .then(() => console.log('DB Connected successfully!'))
     .catch(err => {
         console.log('DB error: ', err.message);
     });
 
-expressConfig(app);
-handlebarsConfig(app);
+
 
 app.use(routes);
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}... `));
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController.js');
 const cubeController = require('./controllers/cubeController.js');
 const accessoryController = require('./controllers/accessoryController');
+const userController = require('./controllers/userController');
 
 
 router.use(homeController);
 //app.get('/', homeController.getHome);
 router.use('/cubes', cubeController);
 router.use('/accessories', accessoryController);
+router.use('/users', userController);
 router.get('/*splat', (req, res) => {
     res.redirect('/404');
 });
